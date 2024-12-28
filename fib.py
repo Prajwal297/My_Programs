b9be13af0aa4d6e1ffa20aacaa0eab43110bfe13
prajwalprabhakar46.com <prajwalprabhakar46.com>
2024-12-28 14:07:35 +0530

    fibbonanci series program

---
+++ fib.py
@@ -0,0 +1,18 @@
+n=int(input("enter the term:"))
+def fib(n) :
+
+    x=0
+    y=1
+    if n<=0:
+        print("enter a positive integer")
+    else:
+        print(x)
+        print(y)      
+    for i in range(2,n):
+        z=x+y
+        x=y
+        y=z
+        print(z)
+fib(n)
+
+   
