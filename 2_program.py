933d892292e23a7207b8726135f60c4a6c063349
prajwalprabhakar46.com <prajwalprabhakar46.com>
2024-12-28 11:53:53 +0530

    added files

---
+++ 2_program.py
@@ -0,0 +1,5 @@
+# this program s to print the tables of entered number
+n=int(input("Enter the number:"))
+for i in range(1,11):
+    print(n*i)
+    
--- python.py
+++ python.py
@@ -1,9 +1,9 @@
-a = int(input("Enter a number"))
+a = int(input("Enter a number :"))
 if (a>0):
-    print("the entered number is positive")
+    print("the entered number is positive.")
 elif(a<0):
-    print("the entered number is negative")
+    print("the entered number is negative.")
 else:
-    print("the entered number is Zero")
+    print("the entered number is Zero.")
 
 
