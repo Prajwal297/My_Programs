fcd3490d53d10d5735a62fe17151c0c526b8436c
prajwalprabhakar46.com <prajwalprabhakar46.com>
2025-01-05 10:21:53 +0530

    added new program

---
+++ largest number.py
@@ -0,0 +1,12 @@
+a= int(input("enter a number : "))
+b = int(input("enter 2nd number : "))
+c= int(input("enter 3rd number : "))
+d= int(input("enter 4th number : "))
+big=a
+if b>big :
+    big=b
+if c>big:
+    big = c
+if d>big:
+    big=d
+print ("largest number is:",big)
